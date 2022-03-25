fea747b27f4e30191161e6ee7753bbcfa5044f82
Jojo0804-hub <60533961+Jojo0804-hub@users.noreply.github.com>
2022-03-25 16:35:26 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+
+var ground2
+var ground, ground_image
+var edges
+var trex ,trex_running;
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
+ground_image=loadImage("ground2.png")
+}
+
+function setup(){
+  createCanvas(600,200)
+  ground2=createSprite(200,190,400,10)
+  //create a trex sprite
+ trex=createSprite(50,185,20,20)
+trex.addAnimation("running",trex_running)
+trex.scale=0.5
+edges = createEdgeSprites()
+ground=createSprite(200,185,400,10)
+ground.velocityX=-4
+ground.x=ground.width/2
+ground.addImage(ground_image)
+ground2.visible=false
+}
+
+function draw(){
+  background("white")
+  if(keyDown("space")&&trex.y>160){
+    trex.velocityY = -10
+  }
+  if(ground.x<30){
+    ground.x=ground.width/2
+  }
+  trex.velocityY = trex.velocityY + 0.8
+  trex.collide(ground2)
+drawSprites()
+}
+ 
